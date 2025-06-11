b696da85748096c212891892f6e59defb4fba96b
sdeats <sdeats12@gmail.com>
2025-06-11 09:42:47 -0500

    updated main to take file input and print file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        print(file_contents)
+def main(filepath):
+    get_book_text(filepath)
+main("books/frankenstein.txt")
