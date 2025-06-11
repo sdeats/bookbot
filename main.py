076d026fd458bb035c2dce90fbb8e8bcd799c740
sdeats <sdeats12@gmail.com>
2025-06-11 11:32:03 -0500

    added sorting feature to letter count and adjusted  where printing messages occur

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from stats import word_count,char_count
+from stats import word_count,char_count,sorted_dict
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         print(file_contents)
 def main(filepath):
-    char_count(filepath)
+    print("======= BOOKBOT =======")
+    print(f"analyzing book found at {filepath}...")
+    print("-------------- Word Count --------------")
+    print(f"found {word_count(filepath)} total words")
+    print("------- Word Count -------")
+    sorted_dict(filepath)
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -4,19 +4,39 @@ def word_count(filepath):
         file_contents = f.read()
         words = file_contents.split()
         num_words = len(words)
-        sentence = f"{num_words} words found in the document"
-        print(sentence)
+    return(num_words)
 def char_count(filepath):
     char_set  = set()
-    char_count = {}
+    char_dict = {}
     with open(filepath) as f:
         file_contents = f.read()
         for letter in file_contents:
             lower_letter = letter.lower()
             if lower_letter in char_set:
-                char_count[lower_letter] +=1
+                char_dict[lower_letter] +=1
             else:
                 char_set.add(lower_letter)
-                char_count[lower_letter] = 1
-        print(char_count)
+                char_dict[lower_letter] = 1
+    return(char_dict)
+def sort_on(dict):
+    return dict["num"]
+def sorted_dict(filepath):
+    char_dict = char_count(filepath)
+    dict_list = []
+    for key in char_dict:
+        sort_dict = {}
+        sort_dict["char"] = key
+        sort_dict["num"] = char_dict[key]
+        dict_list.append(sort_dict)
+    dict_list.sort(reverse=True,key=sort_on)
+    for group in dict_list:
+        if group["char"].isalpha():
+            letter = group["char"]
+            number = group["num"]
+            print(letter,number)
+        else:
+            pass
 
+
+
+    
