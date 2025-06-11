0e1f10114a4f7a4852bdf4128d050cb29720491f
sdeats <sdeats12@gmail.com>
2025-06-11 09:48:43 -0500

    added wordcount function and updated main to only call wordcount

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         print(file_contents)
+def word_count(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        num_words = len(words)
+        sentence = f"{num_words} words found in the document"
+        print(sentence)
 def main(filepath):
-    get_book_text(filepath)
+    word_count(filepath)
 main("books/frankenstein.txt")
